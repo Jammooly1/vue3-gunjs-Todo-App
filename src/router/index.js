5dd95031931f83216114fe551a2795d9ae5afec3
Jammooly1 <dabasjamal@gmail.com>
2022-02-03 04:23:41 -0600

    Added Auth functionality

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/auth/Login.vue'
+import Signup from '../views/auth/Signup.vue'
+import {user, username} from '@/composables/user'
+
+const requireAuth = (to, from, next) => {
+  console.log(user, !user.is, user.is, user)
+  if(!user.is) {
+    next({ name: 'Login' })
+  } else {
+    next()
+  }
+}
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    beforeEnter: requireAuth
   },
   {
     path: '/about',
@@ -13,8 +26,20 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    beforeEnter: requireAuth
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
+    
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: Signup
+  },
 ]
 
 const router = createRouter({
